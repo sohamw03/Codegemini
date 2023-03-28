51018a360a14a5e195a9294c2028c340df86d44e
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-03-29 00:03:11 +0530

    Handled backend of contact page.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 def home(request):
     return render(request, "home/home.html")
 
+
 def contact(request):
-    return render(request, "home/contact.html")
+    thank = False
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        content = request.POST.get("content", "")
+        contact = Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
+        thank = True
+    return render(request, "home/contact.html", {"thank": thank})
+
 
 def about(request):
     return render(request, "home/about.html")
