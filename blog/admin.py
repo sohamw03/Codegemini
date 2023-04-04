44a81dc9f6b8650425d1a0b3c007df9ebea9ed3b
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-04-04 22:57:16 +0530

    Added TinyMCE editor functionality in admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
 # Register your models here.
+admin.site.register(Comment)
+
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+    class Media:
+        js = ("tinyInject.js",)
